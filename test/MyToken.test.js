3b20ddedb94dd1f99b10721a6ae14e02d81c9548
Unmesh28 <unmesh.dabhade@gmail.com>
2022-01-26 19:12:46 +0530

    test case added : It is not possible to send more tokens than total availble

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -7,6 +7,7 @@ chai.use(chaiBN);
 
 var chaiAsPromised = require("chai-as-promised");
 const { assert } = require("console");
+const exp = require("constants");
 const { setMaxListeners } = require("events");
 const { isTypedArray } = require("util/types");
 chai.use(chaiAsPromised);
@@ -23,7 +24,7 @@ contract("Token Test", async(accounts) => {
         //let balance = await instance.balanceOf(accounts[0]);
         //assert.equal(balance.valueOf(), initialSupply.valueOf(), "The balance was not the same");
         expect(await instance.balanceOf(deployedAccount)).to.be.a.bignumber.equal(totalSupply);
-    })
+    });
 
     it("it is possible to send tokens between accounts", async() => {
         const sendTokens = 1;
@@ -33,5 +34,12 @@ contract("Token Test", async(accounts) => {
         expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
         expect(instance.balanceOf(deployedAccount)).to.eventually.to.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
         expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
-    })
+    });
+
+    it("It is not possible to send more tokens than total availble", async() => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployedAccount);
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
+        expect(instance.balanceOf(deployedAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+    });
 });
